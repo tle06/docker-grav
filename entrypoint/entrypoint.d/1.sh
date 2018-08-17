ecb357512be7f61f2ca184936aae5af30096380b
tle06 <tle@tlnk.fr>
2018-08-17 11:08:45 +0200

    update composer install

--- entrypoint/entrypoint.d/1.sh
+++ entrypoint/entrypoint.d/1.sh
@@ -8,7 +8,15 @@ if [ "$(ls -A $DIR)" ]; then
 else
     echo "$DIR is Empty"
     echo "Setup grav"
+    echo "Git clone grav"
     git clone https://github.com/getgrav/grav.git /var/www/grav
+
+    echo "Setup composer"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+
+    echo "Setup grav admin"
     /var/www/grav/bin/grav install
 
     echo "Install grav admin plugin"
