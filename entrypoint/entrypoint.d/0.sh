0c673fe0e79207e70587478a6c0e185b3051c66d
tle06 <tle@tlnk.fr>
2018-08-19 23:22:11 +0200

    fix php config

--- entrypoint/entrypoint.d/0.sh
+++ entrypoint/entrypoint.d/0.sh
@@ -6,8 +6,9 @@ VAR_PHP_MEMORY_LIMIT="${PHP_MEMORY_LIMIT:-128M}"
 VAR_PHP_FILE_UPLOAD="${PHP_FILE_UPLOAD:-On}"
 VAR_PHP_ALLOW_URL="${PHP_ALLOW_URL:-On}"
 #VAR_PHP_MAX_EXCUTION_TIME="${PHP_MAX_EXCUTION_TIME:-30}"
+VAR_PHP_CONFIG_APPLIED="${PHP_CONFIG_APPLIED:-false}"
 
-if [ "$VAR_PHP_CONFIG" = true ]; then
+if [ "${VAR_PHP_CONFIG_APPLIED}" = true ]; then
      echo "PHP config already applied once, upgrade to applied new one"
 else
 
@@ -22,5 +23,9 @@ sed -i -e "s/file_uploads\s*=\s*On/file_uploads = $VAR_PHP_FILE_UPLOAD/g" /etc/p
 echo "[CONFIG] allow_url_fopen with:${VAR_PHP_ALLOW_URL}"
 sed -i -e "s/allow_url_fopen\s*=\s*On/allow_url_fopen = $VAR_PHP_ALLOW_URL/g" /etc/php/7.2/apache2/php.ini
 #sed -i -e "s/max_execution_time\s*=\s*30/max_execution_time = $VAR_PHP_MAX_EXCUTION_TIME/g" /etc/php/7.2/apache2/php.ini
-VAR_PHP_CONFIG = true
+
+VAR_PHP_CONFIG_APPLIED=true
+
 fi
+
+echo "export PHP_CONFIG_APPLIED=${VAR_PHP_CONFIG_APPLIED}" >> ~/.bashrc
